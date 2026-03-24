63b31cd368db5f3dd63937704ea958ff4e38c1af
rukhsana1827 <rukhsana1827asif@gmail.com>
2026-03-24 13:18:39 +0530

    updated site

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'about-us', component: About },
+  { path: 'contact-us', component: Contact },
+  { path: '**', redirectTo: 'home' }
+];
